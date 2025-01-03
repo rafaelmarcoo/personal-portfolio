7d4439d94452389ba57fc9b61e7e59e7efad04bb
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-03 20:57:17 +1300

    Fixing navbar

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,23 @@
+const HomeSection = () => {
+  return (
+    <div className="flex flex-wrap">
+      <div>
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl">Hi! I'm Rafael Marco Manubay</h1>
+        <h2 className="pt-3 text-xl tracking-wide"
+        >
+          Experienced with 
+          <span className="bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent">
+            {" "}
+            Full Stack Development
+          </span>
+        </h2>
+        <h3 className="pt-4 text-2xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi earum fugiat fugit eos omnis consequuntur ullam quibusdam architecto veritatis harum corrupti illo est alias eum placeat, eius in quasi eaque quo perspiciatis adipisci ab. Vel optio, modi minus esse, ratione porro ea culpa error temporibus velit et perspiciatis dolor perferendis?
+        </h3>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default HomeSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,56 @@
+import { useState } from "react"
 import { navItems } from "../constants"
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  return (
-    <nav className="sticky top-0 z-50 border-b py-3 border-neutral-600 backdrop-blur-lg">
-        <div className="container relative px-4 mx-auto">
-            <div className="flex justify-between items-center text-xl">
-                <div className="flex items-center flex-shrink-0">
-                    <span className="text-3xl bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent">Rafael Marco Manubay</span>
-                </div>
-                <ul className="hidden lg:flex ml-5 space-x-10">
-                    {navItems.map((item, index) => (
-                        <li key={index} className="">
-                            <a href={item.href}>{item.label}</a>
-                        </li>
-                    ))}
-                </ul>
-                <div className="hidden lg:flex items-center flex-shrink-0">
-                    <a href="#" className="border border-neutral-700 p-2 rounded-md bg-gradient-to-r from-blue-300 to-blue-600">Download CV</a>
+    const [burgerMenu, setBurgerMenu] = useState(false);
+    const toggleMenuBar = () => {
+        setBurgerMenu(!burgerMenu);
+    };
+
+    return (
+        <nav className="sticky top-0 z-50 border-b py-3 border-neutral-600 backdrop-blur-lg">
+            <div className="container relative px-4 mx-auto">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center flex-shrink-0">
+                        <span className="text-3xl bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent">Rafael Marco Manubay</span>
+                    </div>
+                    <ul className="hidden lg:flex ml-10 space-x-10">
+                        {navItems.map((item, index) => (
+                            <li key={index} className="">
+                                <a href={item.href} className="hover:text-blue-500 transition-colors"
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center flex-shrink-0">
+                        <a href="#" className="border border-neutral-700 p-2 rounded-md bg-gradient-to-r from-blue-300 to-blue-600">Download CV</a>
+                    </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleMenuBar}>
+                            {burgerMenu ? <X /> : <Menu />}
+                        </button>
+                    </div>
+                    {burgerMenu && (
+                        <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                            <ul>
+                                {navItems.map((item, index) => (
+                                    <li key={index} className="py-4">
+                                        <a href={index}>{item.label}</a>
+                                    </li>
+                                ))}
+                                <div>
+                                    <a href="#" className="py-2 px-3 border border-neutral-700 p-2 rounded-md bg-gradient-to-r from-blue-300 to-blue-600">Download CV</a>
+                                </div>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
