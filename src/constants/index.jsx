cddd8ba36b842a29421ef800eb71b7d8a7fd143c
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-03 20:02:35 +1300

    Created constants folder with index.jsx

---
+++ src/constants/index.jsx
@@ -0,0 +1,7 @@
+export const navItems = [
+    { label: "Home", href:"#" },
+    { label: "About", href:"#" },
+    { label: "Projects", href:"#" },
+    { label: "Experience", href:"#" },
+    { label: "Education", href:"#" },
+];
