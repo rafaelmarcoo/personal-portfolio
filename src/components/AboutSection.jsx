73b66a72d750b8f105f2bd60f8a1199d24584678
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-04 17:24:39 +1300

    Added technical skills section in about

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import { hobbies } from "../constants";
 import testBarista from "../assets/hobbies/testBarista.jpg";
 import testGuitar from "../assets/hobbies/testGuitar.jpg";
 import testGym from "../assets/hobbies/testGym.jpg";
+import { techStack, programmingLanguages, databases } from "../constants";
 
 const AboutSection = () => {
   return (
@@ -13,6 +14,43 @@ const AboutSection = () => {
         <h3 className="text-center pt-4 text-lg sm:text-xl p-5">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam voluptatem et nostrum laboriosam qui ut cumque enim consequatur facere? Neque ea ipsum itaque aut incidunt!
         </h3>
+
+        <div className="flex flex-col mb-10 mt-10">
+            <div className="border-t border-neutral-700">
+                <h3 className="text-center pt-4 text-lg sm:text-xl p-5">Main Tech Stack</h3>
+                <div className="flex gap-x-5 gap-y-4 space-x-5 justify-center pb-3">
+                    {techStack.map((url, index) => (
+                        <img src={url} 
+                            alt="logo"
+                            className="w-16" 
+                        />
+                    ))}
+                </div>
+            </div>
+            <div>
+                <h3 className="text-center pt-4 text-lg sm:text-xl p-5">Languages</h3>
+                <div className="flex flex-wrap space-x-5 justify-center gap-x-5 gap-y-4 pb-3">
+                    {programmingLanguages.map((url, index) => (
+                        <img src={url} 
+                            alt="logo"
+                            className="w-16" 
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className="border-b border-neutral-700">
+                <h3 className="text-center pt-4 text-lg sm:text-xl p-5">Databases</h3>
+                <div className="flex gap-x-5 gap-y-4 justify-center pb-3">
+                    {databases.map((url, index) => (
+                        <img src={url} 
+                            alt="logo"
+                            className="w-16" 
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+
         <h2 className="text-2xl sm:text-3xl lg:text-4xl text-center p-5">
                 Other than programming, I am a:
         </h2>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -20,3 +20,26 @@ export const hobbies = [
         description: "Fitness is key to my lifestyle. It keeps me fit and mentally sharp, teaching discipline.",
     }
 ];
+
+export const techStack = [
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
+];
+
+export const programmingLanguages = [
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg',
+];
+
+export const databases = [
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg',
+];
