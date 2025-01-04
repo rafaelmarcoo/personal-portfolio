5f607fac754881c8429a7728fb3a4c3dbc549888
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-04 16:40:08 +1300

    Fixed styling layout of About

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,35 +16,35 @@ const AboutSection = () => {
         <h2 className="text-2xl sm:text-3xl lg:text-4xl text-center p-5">
                 Other than programming, I am a:
         </h2>
-        <div className="flex flex-wrap">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {hobbies.map((hobby, index) => (
-                <div key={index} className="w-full sm:w-1/2 ld:w-1/3 p-2">
-                    <div className="p-10 border border-neutral-700">
-                        <div className="flex items-center">
+                <div key={index} className="p-6 border border-neutral-700 rounded-lg shadow-lg bg-neutral-800">
+                        <div className="flex items-center mb-4">
                             {hobby.title === "Waiter / Barista" && <Coffee />}
                             {hobby.title === "Musician - Guitarist" && <Guitar />}
                             {hobby.title === "Fitness Enthusiast" && <Dumbbell />}
-                            <span className="ml-3 mt-2 mb-2 text-xl">{hobby.title}</span>
+                            <span className="ml-3 mt-2 mb-2 text-xl font-semibold">{hobby.title}</span>
                         </div>
-                        <div className="flex">
-                            <p className="text-md text-neutral-500">{hobby.description}</p>
+                        <div className="flex items-center">
+                            <p className="text-md text-neutral-500 flex-grow">
+                                {hobby.description}
+                            </p>
                             {hobby.title === "Waiter / Barista" && <img 
                                 src={testBarista} 
                                 alt="barista"
-                                className="w-32 h-32 p-3"
+                                className="w-36 h-36 p-3 rounded-full object-cover"
                             />}
                             {hobby.title === "Musician - Guitarist" && <img 
                                 src={testGuitar} 
                                 alt="barista"
-                                className="w-32 h-32 p-3"
+                                className="w-36 h-36 p-3 rounded-full object-cover"
                             />}
                             {hobby.title === "Fitness Enthusiast" && <img 
                                 src={testGym} 
                                 alt="barista"
-                                className="w-32 h-32 p-3"
+                                className="w-36 h-36 p-3 rounded-full object-cover"
                             />}
                         </div>
-                    </div>
                 </div>
             ))}
         </div>
