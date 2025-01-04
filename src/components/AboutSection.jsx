b0ffd5d16b2d7bfb1bbf8a3ba372ef35a5e249a3
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-04 16:28:42 +1300

    Styling of cards

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,9 +1,12 @@
-import { Dumbbell, Guitar, Coffee } from "lucide-react"
-import { hobbies } from "../constants" 
+import { Dumbbell, Guitar, Coffee } from "lucide-react";
+import { hobbies } from "../constants";
+import testBarista from "../assets/hobbies/testBarista.jpg";
+import testGuitar from "../assets/hobbies/testGuitar.jpg";
+import testGym from "../assets/hobbies/testGym.jpg";
 
 const AboutSection = () => {
   return (
-    <div className="mt-16">
+    <div className="mt-16 px-6">
         <h2 className="text-3xl sm:text-4xl lg:text-5xl text-center tracking-wide">
             About me!
         </h2>
@@ -12,19 +15,35 @@ const AboutSection = () => {
         </h3>
         <h2 className="text-2xl sm:text-3xl lg:text-4xl text-center p-5">
                 Other than programming, I am a:
-            </h2>
+        </h2>
         <div className="flex flex-wrap">
             {hobbies.map((hobby, index) => (
                 <div key={index} className="w-full sm:w-1/2 ld:w-1/3 p-2">
                     <div className="p-10 border border-neutral-700">
-                        <div>
-
+                        <div className="flex items-center">
+                            {hobby.title === "Waiter / Barista" && <Coffee />}
+                            {hobby.title === "Musician - Guitarist" && <Guitar />}
+                            {hobby.title === "Fitness Enthusiast" && <Dumbbell />}
+                            <span className="ml-3 mt-2 mb-2 text-xl">{hobby.title}</span>
                         </div>
-                        {hobby.title === "Waiter / Barista" && <Coffee />}
-                        {hobby.title === "Musician - Guitarist" && <Guitar />}
-                        {hobby.title === "Fitness Enthusiast" && <Dumbbell />}
-                        <h3 className="mt-1 mb-2 text-xl">{hobby.title}</h3>
-                        <p className="text-md text-neutral-500">{hobby.description}</p>
+                        <div className="flex">
+                            <p className="text-md text-neutral-500">{hobby.description}</p>
+                            {hobby.title === "Waiter / Barista" && <img 
+                                src={testBarista} 
+                                alt="barista"
+                                className="w-32 h-32 p-3"
+                            />}
+                            {hobby.title === "Musician - Guitarist" && <img 
+                                src={testGuitar} 
+                                alt="barista"
+                                className="w-32 h-32 p-3"
+                            />}
+                            {hobby.title === "Fitness Enthusiast" && <img 
+                                src={testGym} 
+                                alt="barista"
+                                className="w-32 h-32 p-3"
+                            />}
+                        </div>
                     </div>
                 </div>
             ))}
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -9,14 +9,14 @@ export const navItems = [
 export const hobbies = [
     { 
         title: "Waiter / Barista",
-        description: "Working as a barista and waiter isn’t just a job, it’s an opportunity to meet new people, develop communication skills, and work efficiently under pressure. The fast-paced environment has taught me how to handle multiple tasks while maintaining high service quality, all while brewing the perfect cup of coffee!",
+        description: "As a barista, I meet new people, develop communication skills, and multitask under pressure, all while brewing perfect coffee.",
     },
     {
         title: "Musician - Guitarist",
-        description: "Music is more than a hobby for me, it’s my passion and creative outlet. As a guitarist, I love performing live, composing new melodies, and continuously improving my skills. Playing music allows me to express myself in ways words can't and serves as a great stress reliever after a busy day.",
+        description: "Playing guitar is my creative outlet. I perform live, compose melodies, and relieve stress through music.",
     },
     {
         title: "Fitness Enthusiast",
-        description: "I hit the gym regularly because fitness is a crucial part of my lifestyle. It not only keeps me physically fit but also helps me stay mentally sharp and focused. Consistent training has taught me the importance of discipline, goal-setting, and pushing my limits.",
+        description: "Fitness is key to my lifestyle. It keeps me fit and mentally sharp, teaching discipline.",
     }
 ];
